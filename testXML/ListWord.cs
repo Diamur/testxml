a4185a14339d9b75f9338fe19028d683adade2a4
Дмитрий <neelovdima@gmail.com>
2018-11-05 01:06:25 +0300

    Создание и открытие xml

---
+++ testXML/ListWord.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Дмитрий
+ * Дата: 04.11.2018
+ * Время: 14:35
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System;
+//using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace testXML
+{
+	/// <summary>
+	/// Description of ListWord.
+	/// </summary>
+	public class ListWord
+	{
+		string _filePathWord;
+		List<Word> listW = new List<Word>();
+		
+		
+		public ListWord(string filePathWord)
+		{
+			_filePathWord = filePathWord;
+		}
+		
+		
+		public List<Word> GetListWord()
+		{
+			
+						
+			string[] str =  File.ReadAllLines(_filePathWord);
+			var readWord = new string[4]; 			
+		
+			for (int i = 0; i < str.Length  ; i++) {
+				readWord = str[i].Split('\t');				
+			listW.Add(new Word(){Id=i, WordEn=readWord[0], WordTr=readWord[1], WordRu=readWord[2]});
+			}
+				
+			return  listW;	
+			
+		}
+	}
+}
---
+++ testXML/Word.cs
@@ -0,0 +1,24 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: Дмитрий
+ * Дата: 04.11.2018
+ * Время: 10:51
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace testXML
+{
+	/// <summary>
+	/// Description of Word.
+	/// </summary>
+	[Serializable]
+	public class Word
+	{
+	public int Id { get; set; }	
+	public string WordEn { get; set; }
+	public string WordTr { get; set; }
+	public string WordRu { get; set; }
+	}
+}
